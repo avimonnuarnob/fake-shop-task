2300334e5d7d69520349d1a4c9c2101074b5e9a2
Avimonnu Arnob <avimonnuarnob@gmail.com>
2023-01-03 15:28:13 +0600

    change in app.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import React from "react";
-import Header from "./Containers/Header";
-import ProductListing from "./Containers/ProductListing";
-import ProductDetails from "./Containers/ProductDetails";
-// import ProductComponent from './Containers/ProductComponent'
+import React from 'react';
+import Header from './Containers/Header';
+import ProductListing from './Containers/ProductListing';
+import ProductDetails from './Containers/ProductDetails';
 
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 const App = () => {
   return (
     <>
-      {/* <h1>I AM HITKUMAR</h1> */}
       <Router>
         <Header />
 
         <Routes>
-          <Route path="/" exact element={<ProductListing />} />
+          <Route path='/' exact element={<ProductListing />} />
           <Route
-            path="/product/:productId"
+            path='/product/:productId'
             exact
             element={<ProductDetails />}
           />
